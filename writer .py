60d4c3755f6c33dedab662f84df334643a43e1d3
alex3025 <31188915+alex3025@users.noreply.github.com>
2018-05-29 18:24:58 +0200

    Update writer .py

--- writer .py
+++ writer .py
@@ -1,40 +1,42 @@
-print("File Editor\nby alex3025")
-while True:
-	command = input("\nChe cosa vuoi fare? (apri/nuovo/esci): ")
-	try:
-		if command == "esci":
-			break
-		elif command == "apri":
-			chs_1 = input("\nChe file vuoi aprire?: ")
-			file_r = open(chs_1 + ".txt", "r")
-			cont = file_r.read()
-			print(cont)
+print("Writer\nby alex3025")
+def open_file():
+	while True:
+		try:
+			filename_read = input("\nChe file vuoi aprire?: ")
+			file_r = open(filename_read + ".txt", "r")
+			print("\n" + file_r.read() + "\n")
 			file_r.close()
-		elif command == "nuovo":
-			while True:
-				chs_2 = input("\nCome vuoi nominare il file?: ")
-				chs_3 = input("\nIl nome corrente è: " + chs_2 + ". Sei sicuro? (si/no): ")
-				if chs_3 == "no":				
-					chs_2 = input("\nCome vuoi rinominare il file?: ")
-					chs_3 = input("\nIl nome corrente è: " + chs_2 + ". Sei sicuro? (si/no): ")
-				elif chs_3 == "si":
-					print("\nIl nome corrente è: " + chs_2)
-					file_w = open(chs_2 + ".txt", "w")
-					txt = input("\nChe cosa vuoi scrivere nel file?: ")
-					file_w.write(txt)
-					chs_4 = input("\nVuoi salvare? (si/no): ")
-					if chs_4 == "no":
-						txt = input("\nChe cosa vuoi scrivere nel file?: ")
-						file_w.write(txt)
-						chs_4 = input("\nVuoi salvare? (si/no): ")
-					elif chs_4 == "si":
-						file_W.clos()
-						file_w_r = open(chs_2 + ".txt", "r")
-						print("\nEcco cosa hai scritto: " + file_w_r.read())
-						file_w_r.close()
-						print("\nFile salvato con successo!")
-				False
-		else:
-			print("\nComando sconosciuto!\n")
-	except FileNotFoundError:
-		print("\nFile non trovato!")
+		except FileNotFoundError:
+			print("\nNome file errato!")
+def new_file():
+	filename_write = input("\nChe nome vuoi dare al nuovo file?: ")
+	while True:
+		try:
+			confirm = input("\nIl nome sarà: " + filename_write + "\n\nSei sicuro? (si/no): ")
+			if confirm == "si":
+				file_w = open(filename_write + ".txt", "w")
+				text = input("\nChe cosa vuoi scrivere nel file?: ")
+				file_w.write(text)
+				confirm_2 = input("\nVuoi salvare? (si/no): ")
+				if confirm_2 == "no":
+					print("\nIl testo non è stato salvato!\n")
+					break
+				elif confirm_2 == "si":
+					file_w.close()
+					print(" ")
+					break
+			elif confirm == "no":
+				filename_write = input("\nCome vuoi rinominare il file?: ")
+		except:
+			print("\nErrore!")
+while True:
+	try:
+		menu = input("\nChe cosa vuoi fare? (apri/nuovo/esci): ")
+		if menu == "esci":
+			break
+		elif menu == "apri":
+			open_file()
+		elif menu == "nuovo":
+			new_file()
+	except:
+		print("\nErrore!")
